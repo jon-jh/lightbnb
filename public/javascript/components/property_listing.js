20ed4f1e67dbf3b19619b9d3e19594f7e00671ef
jon-jh <hiebert456@gmail.com>
2024-11-08 17:39:53 -0600

    Big style update

--- public/javascript/components/property_listing.js
+++ public/javascript/components/property_listing.js
@@ -1,6 +1,6 @@
 $(() => {
   window.propertyListing = {};
-  
+
   function createListing(property, isReservation) {
     return `
     <article class="property-listing">
@@ -10,22 +10,29 @@ $(() => {
         <section class="property-listing__details">
           <h3 class="property-listing__title">${property.title}</h3>
           <ul class="property-listing__details">
-            <li>number_of_bedrooms: ${property.number_of_bedrooms}</li>
-            <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
-            <li>parking_spaces: ${property.parking_spaces}</li>
+            <li>${property.number_of_bedrooms} 🛌 Bedrooms  </li>
+            <li>${property.number_of_bathrooms} 🛁 Bathrooms  </li>
+            <li>${property.parking_spaces} 🛻 Parking Spots  </li>
           </ul>
-          ${isReservation ? 
-            `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
-            : ``}
+          ${
+            isReservation
+              ? `<p>${moment(property.start_date).format("ll")} - ${moment(
+                  property.end_date
+                ).format("ll")}</p>`
+              : ``
+          }
           <footer class="property-listing__footer">
-            <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
-            <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+            <div class="property-listing__rating">${
+              Math.round(property.average_rating * 100) / 100
+            }/5 stars</div>
+            <div class="property-listing__price">$${
+              property.cost_per_night / 100.0
+            }/night</div>
           </footer>
         </section>
       </article>
-    `
+    `;
   }
 
   window.propertyListing.createListing = createListing;
-
 });
